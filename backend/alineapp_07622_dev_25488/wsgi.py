8bad39179f4e244f5d43e86cc04a1d26478c8bfb
Crowdbotics <engineering@crowdbotics.com>
2022-07-06 20:48:38 +0000

    Initial commit.

---
+++ backend/alineapp_07622_dev_25488/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for alineapp_07622_dev_25488 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alineapp_07622_dev_25488.settings')
+
+application = get_wsgi_application()
